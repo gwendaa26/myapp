336b28896c8b47a3640915a6fca176dc0b12d749
Gwenda Hasnaa <gwendahasnaa26@gmail.com>
2022-09-16 08:59:10 +0700

    create header component and arrange LandingPage

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-import { Link } from 'react-router-dom'
-import propTypes from 'prop-types'
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 
 
 export default function Button(props) {
